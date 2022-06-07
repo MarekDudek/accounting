95fd993a57501551535f954a0b1c50a1babcc614
MarekDudek <marek.dudek@gmail.com>
2022-06-07 20:44:04 +0200

    Fresh Maven project

---
+++ src/test/java/md/accounting/AccountingApplicationTests.java
@@ -0,0 +1,15 @@
+package md.accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
