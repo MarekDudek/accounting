0c2c32991cbd74c4f5c1f88cd8541f6d0004d5a8
MarekDudek <marek.dudek@gmail.com>
2022-07-07 23:05:30 +0200

    broken down

--- src/main/java/md/accounting/AccountingApplication.java
+++ src/main/java/md/accounting/AccountingApplication.java
@@ -1,10 +1,8 @@
 package md.accounting;
 
 import com.google.common.collect.RangeSet;
-import com.google.common.collect.Streams;
 import lombok.extern.slf4j.Slf4j;
-import md.accounting.ipko.AccountHistoryExtractor;
-import md.accounting.ipko.OperationsExtractor;
+import md.accounting.ipko.DataExtractor;
 import md.accounting.ipko.domain.AccountHistory;
 import md.accounting.ipko.domain.Operation;
 import org.javatuples.Triplet;
@@ -18,8 +16,7 @@ import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.List;
 
-import static java.math.BigDecimal.ZERO;
-import static java.util.stream.Collectors.toList;
+import static java.lang.String.format;
 
 @SpringBootApplication
 @Slf4j
@@ -40,45 +37,27 @@ public class AccountingApplication implements CommandLineRunner
         Path directory = new File(args[0]).toPath();
         log.info("Reading from directory {}", directory);
 
-        List<AccountHistory> histories = AccountHistoryExtractor.read(directory);
+        List<AccountHistory> histories = DataExtractor.read(directory);
         log.info("Read {} history files", histories.size());
 
         @SuppressWarnings("UnstableApiUsage")
-        RangeSet<LocalDate> set = AccountHistoryExtractor.range(histories);
+        RangeSet<LocalDate> set = DataExtractor.ranges(histories);
         log.info("Search date ranges {}", set);
 
-        if (!AccountHistoryExtractor.isValid(set)) {
-            throw new IllegalArgumentException(String.format("Range is not valid: %s", set));
+        if (!DataExtractor.isValid(set))
+        {
+            throw new IllegalArgumentException(format("Range is not valid: %s", set));
         }
 
-        List<Operation> operations = OperationsExtractor.extract(histories);
+        List<Operation> operations = DataExtractor.extract(histories);
         log.info("Read {} operations", operations.size());
 
-        List<Triplet<Operation, Operation, BigDecimal>> differences =
-                Streams.zip(
-                        operations.stream(),
-                        operations.stream().skip(1),
-                        (o1, o2) -> {
-                            BigDecimal endingBalance = o1.getEndingBalance().getValue();
-                            BigDecimal startingBalance = o2.getEndingBalance().getValue().subtract(o2.getAmount().getValue());
-                            BigDecimal difference = endingBalance.subtract(startingBalance);
-                            return Triplet.with(o1, o2, difference);
-                        }
-                ).collect(toList());
+        List<Triplet<Operation, Operation, BigDecimal>> differences = DataExtractor.differences(operations);
+        List<Triplet<Operation, Operation, BigDecimal>> inconsistencies = DataExtractor.inconsistencies(differences);
 
-        differences.stream().filter(difference ->
-                difference.getValue2().compareTo(ZERO) != 0
-        ).forEach(diff -> {
-                    log.info("fst {}", diff.getValue0());
-                    log.info("snd {}", diff.getValue1());
-                }
-        );
-
-        List<Triplet<Operation, Operation, BigDecimal>> inconsistencies =
-                differences.stream().filter(difference ->
-                        difference.getValue2().compareTo(ZERO) != 0
-                ).collect(toList());
-
-        log.info("Number of inconsistencies: {}", inconsistencies.size());
+        if (!DataExtractor.noInconsistencies(inconsistencies))
+        {
+            throw new IllegalArgumentException(format("There are inconsistencies: %s", inconsistencies));
+        }
     }
 }
--- src/main/java/md/accounting/ipko/DataExtractor.java
+++ src/main/java/md/accounting/ipko/DataExtractor.java
@@ -2,21 +2,27 @@ package md.accounting.ipko;
 
 import com.google.common.collect.Range;
 import com.google.common.collect.RangeSet;
+import com.google.common.collect.Streams;
 import com.google.common.collect.TreeRangeSet;
 import md.accounting.ipko.domain.AccountHistory;
 import md.accounting.ipko.domain.Date;
+import md.accounting.ipko.domain.Operation;
+import org.javatuples.Triplet;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static java.math.BigDecimal.ZERO;
+import static java.util.Collections.reverse;
 import static java.util.stream.Collectors.toList;
 import static md.accounting.ipko.LocalDateDomain.LocalDates;
 
-public enum AccountHistoryExtractor
+public enum DataExtractor
 {
     ;
 
@@ -31,7 +37,7 @@ public enum AccountHistoryExtractor
     }
 
     @SuppressWarnings("UnstableApiUsage")
-    public static RangeSet<LocalDate> range(List<AccountHistory> histories)
+    public static RangeSet<LocalDate> ranges(List<AccountHistory> histories)
     {
         Stream<Date> ds =
                 histories.stream().map(h ->
@@ -62,5 +68,41 @@ public enum AccountHistoryExtractor
     public static boolean isValid(RangeSet<LocalDate> set)
     {
         return set.asRanges().size() == 1;
+    }
+
+    public static List<Operation> extract(List<AccountHistory> histories)
+    {
+        return histories.stream().flatMap(h -> {
+                    List<Operation> ops = h.getOperations().getOperation();
+                    reverse(ops);
+                    return ops.stream();
+                }
+        ).collect(toList());
+    }
+
+    public static List<Triplet<Operation, Operation, BigDecimal>> differences(List<Operation> operations)
+    {
+        return Streams.zip(
+                operations.stream(),
+                operations.stream().skip(1),
+                (o1, o2) -> {
+                    BigDecimal endingBalance = o1.getEndingBalance().getValue();
+                    BigDecimal startingBalance = o2.getEndingBalance().getValue().subtract(o2.getAmount().getValue());
+                    BigDecimal d = endingBalance.subtract(startingBalance);
+                    return Triplet.with(o1, o2, d);
+                }
+        ).collect(toList());
+    }
+
+    public static List<Triplet<Operation, Operation, BigDecimal>> inconsistencies(List<Triplet<Operation, Operation, BigDecimal>> differences)
+    {
+        return differences.stream().filter(d ->
+                d.getValue2().compareTo(ZERO) != 0
+        ).collect(toList());
+    }
+
+    public static boolean noInconsistencies(List<Triplet<Operation, Operation, BigDecimal>> inconsistencies)
+    {
+        return inconsistencies.isEmpty();
     }
 }
